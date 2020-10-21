f9ae6c3251c2db88c5c0951b859eaf6138acb693
matt szymanski <matthew.szymanski323@gmail.com>
2020-10-21 10:13:49 -0400

    created 1st production build

--- src/components/App.js
+++ src/components/App.js
@@ -94,6 +94,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        
         <Form
           handleChangeProps={this.handleChange}
           handleSubmitProps={this.onSubmit}
